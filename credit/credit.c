a9f4b3757fed3d2ebca7a68b5a84adc3149b501e
Blezerker <77137812+Blezerker@users.noreply.github.com>
2022-02-10 18:41:22 +0000

    Thu, Feb 10, 2022, 6:41 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    do
+    {
+        int height = get_int("Height: ");
+    }
+    while(height < 1 || height > 8);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0 )
+    }
 }
