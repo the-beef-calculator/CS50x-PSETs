8a523c69fe758154dac510be87e74be6ba55e12a
Blezerker <77137812+Blezerker@users.noreply.github.com>
2022-02-11 00:13:22 +0000

    Fri, Feb 11, 2022, 12:13 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,1 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    int height;
+    int spaces;
+
+    do
+    {
+        height = get_int("Height: ");
+    }
+
+    while(height < 1 || height > 8);
+
+    // starting on left side.
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = height; j < (height - 1) - 1; j++ )
+        {
+            print(" ");
+        }
+
+    }
+}
