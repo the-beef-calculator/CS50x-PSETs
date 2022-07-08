dffce3731d80eee4b5882081a47a3d0d2689d903
Blezerker <77137812+Blezerker@users.noreply.github.com>
2022-07-08 01:03:35 +0000

    Fri, Jul 8, 2022, 1:03 AM +00:00

---
+++ credit/credit.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+
+int main(void)
+{
+    do
+    {
+        int height = get_int("Height: ");
+    }
+    while(height < 1 || height > 8);
+
+
+    //printing each row
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+
+        }
+}
+
+
