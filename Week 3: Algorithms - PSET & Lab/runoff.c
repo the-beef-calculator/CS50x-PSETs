a9e027fd5cab292e873b5029e78c1031a520c721
Blezerker <77137812+Blezerker@users.noreply.github.com>
2022-09-29 22:57:01 +0000

    Thu, Sep 29, 2022, 10:57 PM +00:00

--- Week 3: Algorithms - PSET & Lab/runoff.c
+++ Week 3: Algorithms - PSET & Lab/runoff.c
@@ -144,14 +144,14 @@ void tabulate(void)
 {
     for (int i = 0; i < voter_count; i++)
     {
-        for(int j = 0; j < ; j++)
+        for(int j = 0; j < preferences[0].length; j++)
         {
             //store the value in the array into a variable in order to properly reference the candidate
             int p = preferences[i][j];
 
             if (candidates[p].eliminated == false)
             {
-                
+
             }
 
 
