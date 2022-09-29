9423b9796d8c2250aa88c328d52dc6f2bfa3e130
Blezerker <77137812+Blezerker@users.noreply.github.com>
2022-09-29 20:51:48 +0000

    Thu, Sep 29, 2022, 8:51 PM +00:00

--- Week 3: Algorithms - PSET & Lab/runoff.c
+++ Week 3: Algorithms - PSET & Lab/runoff.c
@@ -146,7 +146,7 @@ void tabulate(void)
     {
         for(int j = 0; j < preferences[i][j].length; j++)
         {
-            if (preferences[i][j)
+            if ()
         }
     }
     return;
