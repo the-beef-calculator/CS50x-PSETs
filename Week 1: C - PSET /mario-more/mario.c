d97c0a67ec93e9087c8cd777990aa6397e08afb3
Blezerker <77137812+Blezerker@users.noreply.github.com>
2022-09-29 03:44:51 +0000

    Thu, Sep 29, 2022, 3:44 AM +00:00

--- Week 1: C - PSET /mario-more/mario.c
+++ Week 1: C - PSET /mario-more/mario.c
@@ -16,7 +16,12 @@ int main(void)
 
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; )
+        for (int j = 0; j < height; j++){
+            while (j < height-1){
+                printf(" ");
+            }
+            printf("#");
+        }
 
 
     }
