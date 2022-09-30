5d706673c54f4a267f5af3d2a0662aab0f8b5ca2
Blezerker <77137812+Blezerker@users.noreply.github.com>
2022-09-30 00:42:19 +0000

    Fri, Sep 30, 2022, 12:42 AM +00:00

--- Week 3: Algorithms - PSET & Lab/runoff.c
+++ Week 3: Algorithms - PSET & Lab/runoff.c
@@ -207,11 +207,12 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
+    int checker;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes == min)
         {
-            
+            checker +=min;
         }
     }
     return false;
