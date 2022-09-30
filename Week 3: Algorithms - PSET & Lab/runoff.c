d79a129e90e39ce0a55a0fd5f16e1b6b7f19b1e6
Blezerker <77137812+Blezerker@users.noreply.github.com>
2022-09-30 00:11:23 +0000

    Fri, Sep 30, 2022, 12:11 AM +00:00

--- Week 3: Algorithms - PSET & Lab/runoff.c
+++ Week 3: Algorithms - PSET & Lab/runoff.c
@@ -193,7 +193,13 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
+    int minimum;
+    for (int i = 0; i < candidates.length; i++)
+    {
+        
+
+        if (candidates[i].votes <= minimum)
+    }
     return 0;
 }
 
