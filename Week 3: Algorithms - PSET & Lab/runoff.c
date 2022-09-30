c90a682879cc207e325f90cd5e68c9e37e5620d9
Blezerker <77137812+Blezerker@users.noreply.github.com>
2022-09-30 00:59:06 +0000

    Fri, Sep 30, 2022, 12:59 AM +00:00

--- Week 3: Algorithms - PSET & Lab/runoff.c
+++ Week 3: Algorithms - PSET & Lab/runoff.c
@@ -179,7 +179,7 @@ bool print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes > (voter_count/2))
+        if (candidates[i].votes => (voter_count/2))
         {
             printf("%s", candidates[i].name);
             return true;
