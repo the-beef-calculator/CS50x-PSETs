7171ae0a69c9ca9f08b0883ee6dde565160c2104
Blezerker <77137812+Blezerker@users.noreply.github.com>
2022-09-30 21:14:44 +0000

    Fri, Sep 30, 2022, 9:14 PM +00:00

--- Week 3: Algorithms - PSET & Lab/runoff.c
+++ Week 3: Algorithms - PSET & Lab/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidates[i].name) == 0)
+        if (strcmp(name , candidates[i].name) == 0)
         {
             preferences[voter][rank] = i;
             return true;
