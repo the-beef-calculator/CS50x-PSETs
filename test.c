2d81c326ea08abce165f0be89d01fc0697b4403f
Blezerker <77137812+Blezerker@users.noreply.github.com>
2022-10-08 06:13:27 +0000

    Sat, Oct 8, 2022, 6:13 AM +00:00

---
+++ test.c
@@ -0,0 +1,14 @@
+
+
+int main (void){
+
+
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    char str[50];
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%s", str);
+    }
+}
