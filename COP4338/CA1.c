0cf40d1b1af946ccf167f60851ae479f5de7ba75
Blezerker <77137812+Blezerker@users.noreply.github.com>
2023-02-23 06:26:58 +0000

    Thu, Feb 23, 2023, 1:26 AM -05:00

--- COP4338/CA1.c
+++ COP4338/CA1.c
@@ -282,7 +282,7 @@ int main(int argc, char* argv[])
 
 			if(result)
 			{
-				printf("%s%d%s\n", number? line_no:"", index, lineptr[j]);
+				printf("%s@:%d %s\n", number? line_no:"", index, lineptr[j]);
 			}
 
 		}
