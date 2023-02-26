d6758b6ae8f016f72c45f1a68a72620d82795023
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-02-26 23:59:38 +0000

    Sun, Feb 26, 2023, 6:59 PM -05:00

--- random/add.c
+++ random/add.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 
-int add(int x, int y)
-{
-    return x+y;
-}
+
 int main(void)
 {
     int x = 104;
     int y = 3000;
+    int z = 0;
 
-    printf("%d\n", add (x,y));
+    z = x+y;
+
+    printf("%d\n", z);
 
     return 0;
 }
