546b8c01a6b72a2b7419d3f7b5287b8c865b737b
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-04-28 05:14:12 +0000

    Fri, Apr 28, 2023, 1:14 AM -04:00

--- random_puzzle_generator.c
+++ random_puzzle_generator.c
@@ -3,7 +3,7 @@
 #include<string.h>
 #include<time.h>
 #define error(m, c) do{fprintf(stderr, "%s\n", m);exit(c);}while(0)
-void main(int argc, char** argv){
+int main(int argc, char** argv){
 	if(argc != 3)
 		error("Fatal error. Usage: puzzle row_no col_no", 1);
 	long n = atoi(argv[1]);
