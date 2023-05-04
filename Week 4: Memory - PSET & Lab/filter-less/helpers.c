b68446a25f7cefe3447424fa6277a4b3111ece81
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-04 03:03:38 +0000

    Wed, May 3, 2023, 11:03 PM -04:00

---
+++ Week 4: Memory - PSET & Lab/filter-less/helpers.c
@@ -0,0 +1,79 @@
+#include "helpers.h"
+#include <stdio.h>
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+
+   BYTE grayscaleHex;
+
+   for (int i = 0; i < height; i++)
+   {
+        for (int j = 0; j < width; j++)
+        {
+            //grayscale is defined by averaging R,G, and B colors, and changing every pixel hex color to the new gray hex value.
+            grayscaleHex = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+            image[i][j].rgbtRed = grayscaleHex;
+            image[i][j].rgbtGreen = grayscaleHex;
+            image[i][j].rgbtBlue = grayscaleHex;
+
+        }
+   }
+
+
+
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    BYTE sepiaRed,sepiaGreen,sepiaBlue;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+           float tr = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+           sepiaRed = (tr > 255) ? 255 : tr;
+
+           float tg = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+           sepiaGreen = (tg > 255) ? 255 : tg;
+
+           float tb = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+           sepiaBlue = (tb > 255) ? 255 : tb;
+
+
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE tmp;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            tmp.rgbtRed = image[i][j].rgbtRed;
+            tmp.rgbtGreen = image[i][j].rgbtGreen;
+            tmp.rgbtBlue = image[i][j].rgbtBlue;
+
+            
+
+        }
+    }
+
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
