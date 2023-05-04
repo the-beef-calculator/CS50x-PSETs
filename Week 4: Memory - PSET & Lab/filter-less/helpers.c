b4c00ce275d4d059995ee2baa980d051cc7e2c33
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-04 03:55:45 +0000

    Wed, May 3, 2023, 11:55 PM -04:00

--- Week 4: Memory - PSET & Lab/filter-less/helpers.c
+++ Week 4: Memory - PSET & Lab/filter-less/helpers.c
@@ -62,9 +62,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             //copy of first
-            tmp.rgbtRed = image[i][j].rgbtRed;
-            tmp.rgbtGreen = image[i][j].rgbtGreen;
-            tmp.rgbtBlue = image[i][j].rgbtBlue;
+            tmp.rgbtRed = *image[i][j].rgbtRed;
+            tmp.rgbtGreen = *image[i][j].rgbtGreen;
+            tmp.rgbtBlue = *image[i][j].rgbtBlue;
 
 
             image[i][j].rgbtRed = image[i][width - j].rgbtRed;
@@ -75,8 +75,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             image[i][width - j].rgbtBlue = tmp.rgbtBlue;
             image[i][width - j].rgbtGreen = tmp.rgbtGreen;
 
-            swap(i, j, &image[i][j], &image[i][j-1]);
-
+            
 
 
         }
