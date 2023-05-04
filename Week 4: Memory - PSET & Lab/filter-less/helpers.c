f3a3e6bc0e9a16f6b0737ad57b2bd2907f660685
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-04 21:16:51 +0000

    Thu, May 4, 2023, 5:16 PM -04:00

--- Week 4: Memory - PSET & Lab/filter-less/helpers.c
+++ Week 4: Memory - PSET & Lab/filter-less/helpers.c
@@ -83,7 +83,15 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE 
+    RGBTRIPLE copy;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+        }
+    }
     return;
 }
 
