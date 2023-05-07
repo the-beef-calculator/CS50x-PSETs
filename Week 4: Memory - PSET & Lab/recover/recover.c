0efbc16ec76da2671e6ba680c3c1bde2e3cb3df2
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-07 03:41:40 +0000

    Sat, May 6, 2023, 11:41 PM -04:00

--- Week 4: Memory - PSET & Lab/recover/recover.c
+++ Week 4: Memory - PSET & Lab/recover/recover.c
@@ -3,19 +3,23 @@
 
 int main(int argc, char *argv[])
 {
+    // checking for proper usage
     if (argc != 2)
     {
-        printf("usage: ./recover FILENAME.raw");
+        printf("Usage: ./recover FILENAME.raw \n");
         return 1;
     }
+
 
     FILE *f = fopen(argv[1],"r");
-
-    if (*f == NULL)
+    //checking if file was successfully located and opened
+    if (f == NULL)
     {
-        printf("Invalid file.");
+        printf("File not found or is invalid. \n");
         return 1;
     }
+
+
 
 
 }
