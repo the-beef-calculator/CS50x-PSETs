dc8208fca8c34dc8f2e1b586ef50517be74e515b
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-08 15:11:37 +0000

    Mon, May 8, 2023, 11:11 AM -04:00

--- Week 4: Memory - PSET & Lab/recover/recover.c
+++ Week 4: Memory - PSET & Lab/recover/recover.c
@@ -36,10 +36,13 @@ int main(int argc, char *argv[])
 
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
         {
-            
+
+            c++;
         }
 
     }
+
+    printf("found %d files!", c);
 
     free(filename);
 
