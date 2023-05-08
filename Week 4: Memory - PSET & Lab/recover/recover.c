475d1fb6d0269c3a4eb0c18e95e6dd3ebd6dc45c
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-08 15:20:15 +0000

    Mon, May 8, 2023, 11:20 AM -04:00

--- Week 4: Memory - PSET & Lab/recover/recover.c
+++ Week 4: Memory - PSET & Lab/recover/recover.c
@@ -36,7 +36,10 @@ int main(int argc, char *argv[])
 
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            sprintf(filename, "%03i.jpg")
+            sprintf(filename, "%03i.jpg", c);
+            FILE *img = fopen(filename,"w");
+
+            fwrite(img,)
 
 
         }
