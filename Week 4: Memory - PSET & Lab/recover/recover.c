7225155ed60960b208042771246e2254d97e41cb
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-08 15:13:28 +0000

    Mon, May 8, 2023, 11:13 AM -04:00

--- Week 4: Memory - PSET & Lab/recover/recover.c
+++ Week 4: Memory - PSET & Lab/recover/recover.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
     {
         //printf("%d %d %d\n ", buffer[0] ,buffer[1], buffer[2]);
 
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
 
             c++;
