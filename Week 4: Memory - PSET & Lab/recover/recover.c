d10188dc6f4d8ce42d8b7e011c16de12e1d670aa
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-08 15:25:36 +0000

    Mon, May 8, 2023, 11:25 AM -04:00

--- Week 4: Memory - PSET & Lab/recover/recover.c
+++ Week 4: Memory - PSET & Lab/recover/recover.c
@@ -32,9 +32,10 @@ int main(int argc, char *argv[])
 
     while(fread(buffer, 1, BLOCK_SIZE, f) == BLOCK_SIZE)
     {
-        //printf("%d %d %d\n ", buffer[0] ,buffer[1], buffer[2]);
+        
 
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+
         {
             sprintf(filename, "%03i.jpg", c);
             FILE *img = fopen(filename,"w");
