3f8fddd6e0871756b685799cf7f6dadd13d757da
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-09 22:07:18 +0000

    Tue, May 9, 2023, 6:07 PM -04:00

--- Week 5: Data Sructures - PSET & Lab/inheritance/inheritance.c
+++ Week 5: Data Sructures - PSET & Lab/inheritance/inheritance.c
@@ -90,11 +90,12 @@ void free_family(person *p)
         return;
     }
 
-
     // TODO: Free parents recursively
     if (p->parents[0] != NULL || p->parents[1] != NULL)
     {
         free_family(p->parents[0]);
+
+        free_family(p->parents[1]);
     }
     // TODO: Free child
     else
