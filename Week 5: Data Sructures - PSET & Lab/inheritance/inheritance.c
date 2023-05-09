d60737f3fccf068cac60ea46bd214a37e1472c8c
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-09 20:03:44 +0000

    Tue, May 9, 2023, 4:03 PM -04:00

--- Week 5: Data Sructures - PSET & Lab/inheritance/inheritance.c
+++ Week 5: Data Sructures - PSET & Lab/inheritance/inheritance.c
@@ -50,7 +50,9 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        
+
+        parent0->
+
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
