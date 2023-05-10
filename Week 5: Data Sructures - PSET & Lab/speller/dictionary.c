08de6bdefb4a489f66956da6d5807460adea09c2
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-10 23:08:16 +0000

    Wed, May 10, 2023, 7:08 PM -04:00

--- Week 5: Data Sructures - PSET & Lab/speller/dictionary.c
+++ Week 5: Data Sructures - PSET & Lab/speller/dictionary.c
@@ -30,7 +30,7 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
+   
     return toupper(word[0]) - 'A';
 }
 
