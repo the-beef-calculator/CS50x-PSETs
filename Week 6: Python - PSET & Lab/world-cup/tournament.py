61c4fdd424c9b6e699baf345eafe1b2e1ebcb076
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-13 03:53:10 +0000

    Fri, May 12, 2023, 11:53 PM -04:00

--- Week 6: Python - PSET & Lab/world-cup/tournament.py
+++ Week 6: Python - PSET & Lab/world-cup/tournament.py
@@ -42,7 +42,7 @@ def main():
 
     while(tournament_count != 0):
         winner = simulate_tournament(teams)
-        counts[winner[0][0]["team"]] +=1
+        counts[winner[0]["team"]] +=1
         tournament_count -=1
 
 
@@ -83,7 +83,7 @@ def simulate_tournament(teams):
     while len(winner[0]) > 1:
         winner = [simulate_round(winner[0])]
 
-    return winner
+    return winner[0]
 
 if __name__ == "__main__":
     main()
