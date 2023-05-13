a6e5e254e95a3d1f92e861a7fd28a23aa757099d
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-13 04:30:03 +0000

    Sat, May 13, 2023, 12:30 AM -04:00

--- Week 6: Python - PSET & Lab/sentimental-hello/hello.py
+++ Week 6: Python - PSET & Lab/sentimental-hello/hello.py
@@ -1,4 +1,5 @@
 from cs50 import get_string
 
-name = get_string("ARRRR What they be callin 'ye?")
-print("Ahoy" + name + "!")
+#prompts for a string
+name = get_string("ARRRR What they be callin 'ye?\n")
+print("Ahoy " + name + "!")
