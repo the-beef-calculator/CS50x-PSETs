8157816a2eef5c8db7a6bdfe0e3514a56a2fa0c1
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-13 16:38:00 +0000

    Sat, May 13, 2023, 12:38 PM -04:00

--- Week 6: Python - PSET & Lab/sentimental-mario-more/mario.py
+++ Week 6: Python - PSET & Lab/sentimental-mario-more/mario.py
@@ -6,9 +6,11 @@ def main():
     while (height < 1 or height > 8):
         height = get_int("Height: ")
 
+    # there will always be at least one hash. spaces will be one less than height
     spaces = height - 1
     hashnumber = 1
 
+    #building the pyramid
     for i in range(0, height):
         for j in range(0, spaces):
             print(" ",end="")
@@ -16,17 +18,13 @@ def main():
             print("#",end="")
 
         print("  ",end="")
-
+        #end keyword to modify print to not add a new line
         for z in range(0, hashnumber):
             print("#",end="")
         print("")
 
+        #incrementing and decrementing
         hashnumber += 1
         spaces -=1
-
-
-
-
-
 
 main()
