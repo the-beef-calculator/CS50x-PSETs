1f5b404802daf97452ee2bfcb020423ecd8aba69
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-13 04:54:15 +0000

    Sat, May 13, 2023, 12:54 AM -04:00

--- Week 6: Python - PSET & Lab/sentimental-mario-more/mario.py
+++ Week 6: Python - PSET & Lab/sentimental-mario-more/mario.py
@@ -3,7 +3,7 @@ from cs50 import get_int
 def main():
     height = get_int("Height: ")
 
-    while (height < 1 and height > 8):
+    while (height < 1 or height > 8):
         height = get_int("Height: ")
 
 
