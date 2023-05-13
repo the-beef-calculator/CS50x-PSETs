5364755a15ab5c87012a1b484bc8e292eed80a34
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-13 16:32:53 +0000

    Sat, May 13, 2023, 12:32 PM -04:00

--- Week 6: Python - PSET & Lab/sentimental-mario-more/mario.py
+++ Week 6: Python - PSET & Lab/sentimental-mario-more/mario.py
@@ -7,11 +7,24 @@ def main():
         height = get_int("Height: ")
 
     spaces = height - 1
+    hashnumber = 1
 
     for i in range(0, height):
         for j in range(0, spaces):
-            print(" "end="")
-        
+            print(" ",end="")
+        for z in range(0, hashnumber):
+            print("#",end="")
+
+        print("  ",end="")
+
+        for z in range(0, hashnumber):
+            print("#",end="")
+        print("")
+
+        hashnumber += 1
+        spaces -=1
+
+
 
 
 
