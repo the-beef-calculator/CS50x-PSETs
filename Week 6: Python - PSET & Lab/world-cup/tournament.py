6213ddbb190c897b11f1d1ac1390c88c60875ae2
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-13 01:49:19 +0000

    Fri, May 12, 2023, 9:49 PM -04:00

--- Week 6: Python - PSET & Lab/world-cup/tournament.py
+++ Week 6: Python - PSET & Lab/world-cup/tournament.py
@@ -35,6 +35,9 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    for team in teams:
+        
+
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
