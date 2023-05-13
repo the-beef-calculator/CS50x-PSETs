123d3f712e7ad2885cac61fdd0ca70cc780ec467
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-13 00:41:42 +0000

    Fri, May 12, 2023, 8:41 PM -04:00

--- Week 6: Python - PSET & Lab/world-cup/tournament.py
+++ Week 6: Python - PSET & Lab/world-cup/tournament.py
@@ -17,7 +17,7 @@ def main():
     teams = []
     # TODO: Read teams into memory from file
     with open(sys.argv[1], "r") as file:
-        reader = csv.reader(file)
+        reader = csv.DictReader(file)
         next(reader)
 
         for row in reader:
