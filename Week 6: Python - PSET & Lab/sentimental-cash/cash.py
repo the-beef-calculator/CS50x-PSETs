f43ad53fd6bbcda04e9732e4ce2fc5df90dbcce5
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-16 23:31:31 +0000

    Tue, May 16, 2023, 7:31 PM -04:00

--- Week 6: Python - PSET & Lab/sentimental-cash/cash.py
+++ Week 6: Python - PSET & Lab/sentimental-cash/cash.py
@@ -2,6 +2,7 @@ from cs50 import get_float
 
 def main():
 
+    #initiate relevant variables
     coinTotal = 0
     coins = []
 
@@ -9,29 +10,32 @@ def main():
 
     while (cents < 0):
         cents = get_float("Change owed: ")
-
+    #populate array with coin amount, then subtract coin value from total
     quarterCoins = get_quarterCoins(cents) * 0.25
     coins.append(get_quarterCoins(cents))
     cents = round((cents - quarterCoins), 2)
 
+    #repeat
     dimeCoins = get_dimeCoins(cents) * 0.1
     coins.append(get_dimeCoins(cents))
     cents = round((cents - dimeCoins), 2)
 
+    #repeat
     nickelCoins = get_nickelCoins(cents) * 0.05
     coins.append(get_nickelCoins(cents))
     cents = round((cents - nickelCoins), 2)
 
+    #repeat
     pennyCoins = get_pennyCoins(cents) * 0.01
     coins.append(get_pennyCoins(cents))
     cents = round((cents - pennyCoins), 2)
 
 
-
+    #add up coins
     for i in range(len(coins)):
         coinTotal += coins[i]
 
-
+    #print to console
     print(coinTotal)
 
 
