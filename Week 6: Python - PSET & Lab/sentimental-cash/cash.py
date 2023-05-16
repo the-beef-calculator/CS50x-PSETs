32338eb35b5ba694dcf960372b1896ee80f3dd8b
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-16 22:07:14 +0000

    Tue, May 16, 2023, 6:07 PM -04:00

--- Week 6: Python - PSET & Lab/sentimental-cash/cash.py
+++ Week 6: Python - PSET & Lab/sentimental-cash/cash.py
@@ -3,5 +3,16 @@ from cs50 import get_int
 def main():
 
     cents = get_int("How many cents?")
+
+    while (cents < 0):
+        cents = get_int("How many cents?")
+
     
+
+    quarterCoins = (cents / 25)
+    dimeCoins = (cents / 10 )
+    nickelCoins = (cents / 5)
+    pennyCoins = (cents / 1)
+
+
 
