7958634d6381573eb178a2b10dc016c4fb753511
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-16 23:22:27 +0000

    Tue, May 16, 2023, 7:22 PM -04:00

--- Week 6: Python - PSET & Lab/sentimental-cash/cash.py
+++ Week 6: Python - PSET & Lab/sentimental-cash/cash.py
@@ -3,7 +3,7 @@ from cs50 import get_float
 def main():
 
     coinTotal = 0
-    coins
+    coins = []
 
     cents = get_float("Change owed: ")
 
@@ -11,16 +11,19 @@ def main():
         cents = get_float("Change owed: ")
 
     quarterCoins = get_quarterCoins(cents) * 0.25
-
+    coins.append(quarterCoins)
     cents = round((cents - quarterCoins), 2)
 
     dimeCoins = get_dimeCoins(cents) * 0.1
+    coins.append(dimeCoins)
     cents = round((cents - dimeCoins), 2)
 
     nickelCoins = get_nickelCoins(cents) * 0.05
+    coins.append(nickelCoins)
     cents = round((cents - nickelCoins), 2)
 
     pennyCoins = get_pennyCoins(cents) * 0.01
+    coins.append(pennyCoins)
     cents = round((cents - pennyCoins), 2)
 
 
