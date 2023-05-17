373158ad6067b9ee1d5c3ee7c6f8abcd96c0fea8
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-17 05:14:21 +0000

    Wed, May 17, 2023, 1:14 AM -04:00

--- Week 6: Python - PSET & Lab/sentimental-readability/readability.py
+++ Week 6: Python - PSET & Lab/sentimental-readability/readability.py
@@ -1,16 +1,20 @@
 from cs50 import get_string
+import re
 
 # 0.0588 * L - 0.296 * S - 15.8
 
 def main():
 
+    #recieve input from user
     input = get_string("Text: ")
 
+    #calculations based on given equation
     L = ((count_letters(input) / count_words(input)) * 100)
     S = ((count_sentences(input) / count_words(input) * 100 ))
 
     grade_level = (0.0588 * L - 0.296 * S - 15.8)
 
+    #printing output 
     if (round(grade_level) > 16):
         print("Grade 16+")
     elif(round(grade_level) < 0):
@@ -31,7 +35,9 @@ def count_words(sentence):
 
 def count_sentences(input):
 
+    #re module. Provides pattern matching capabilities.
     punctuation = r"[.!?]+"
+    #findall finds all occurrents of (punctuation) in (input)
     sentences = len(re.findall(punctuation,input))
     return sentences
 
