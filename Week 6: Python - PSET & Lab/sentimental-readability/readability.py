ea2704cd73500668155a454a0f39f4a8015568f2
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-17 05:03:34 +0000

    Wed, May 17, 2023, 1:03 AM -04:00

--- Week 6: Python - PSET & Lab/sentimental-readability/readability.py
+++ Week 6: Python - PSET & Lab/sentimental-readability/readability.py
@@ -9,7 +9,15 @@ def main():
     L = ((count_letters(input) / count_words(input)) * 100)
     S = ((count_sentences(input) / count_words(input) * 100 ))
 
-    
+    grade_level = (0.0588 * L - 0.296 * S - 15.8)
+
+    if (round(grade_level) > 16):
+        print("Grade 16+")
+    elif(round(grade_level) < 0):
+        print("Before Grade 1")
+    else:
+        print("Grade " + round(grade_level))
+
 
 
 def count_letters(sentence):
@@ -28,3 +36,5 @@ def count_sentences(input):
     sentences += input.count("?")
 
     return sentences
+
+main()
