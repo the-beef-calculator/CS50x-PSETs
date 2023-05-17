6fca0fb51f5718fccb7ba007d95bada85b075f1d
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-17 20:49:48 +0000

    Wed, May 17, 2023, 4:49 PM -04:00

--- Week 6: Python - PSET & Lab/dna/dna.py
+++ Week 6: Python - PSET & Lab/dna/dna.py
@@ -5,7 +5,8 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    
+    if (len(sys.argv) != 3):
+        
     # TODO: Read database file into a variable
 
     # TODO: Read DNA sequence file into a variable
