5443258cbffdcaa12c5e4b4b5d097dd13b3fd6b1
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-19 03:14:56 +0000

    Thu, May 18, 2023, 11:14 PM -04:00

--- Week 7: SQL - PSET & Lab/movies/11.sql
+++ Week 7: SQL - PSET & Lab/movies/11.sql
@@ -2,3 +2,5 @@ In 11.sql, write a SQL query to list the titles of the five highest rated movies
 
     Your query should output a table with a single column for the title of each movie.
     You may assume that there is only one person in the database with the name Chadwick Boseman.
+
+SELECT title FROM movies JOIN ratings ON movies.id = movie_id JOIN stars ON movies.id = movie_id JOIN people ON stars.person_id = people.id WHERE name = "Chadwick Boseman" ORDER BY rating;
