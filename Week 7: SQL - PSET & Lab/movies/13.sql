9623948590e4698179f1b6c7e874312330626958
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-19 03:53:42 +0000

    Thu, May 18, 2023, 11:53 PM -04:00

---
+++ Week 7: SQL - PSET & Lab/movies/13.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT p.name
+FROM people p
+JOIN stars s ON p.id = s.person_id
+JOIN movies m ON s.movie_id = m.id
+JOIN stars s2 ON m.id = s2.movie_id
+JOIN people p2 ON s2.person_id = p2.id
+JOIN movies m2 ON s2.movie_id = m2.id
+JOIN stars s3 ON m2.id = s3.movie_id
+JOIN people p3 ON s3.person_id = p3.id
+WHERE p2.name = 'Kevin Bacon'
+  AND p2.birth = 1958
+  AND p3.id <> p.id;
