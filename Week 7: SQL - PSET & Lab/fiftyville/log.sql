65ba9bee6140c14a12566bc91a078f55bd5b1025
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-19 18:47:49 +0000

    Fri, May 19, 2023, 2:47 PM -04:00

--- Week 7: SQL - PSET & Lab/fiftyville/log.sql
+++ Week 7: SQL - PSET & Lab/fiftyville/log.sql
@@ -1,2 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Crime info: Took place on July 28, 2021 on Humphrey Street.
+-- 1st Query: .schema to get an idea of what i'm workin with
+-- 2nd Query:
