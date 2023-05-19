d449ba4fc6167b8235d994ca6e19d5ae7a0169b7
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-19 03:57:57 +0000

    Thu, May 18, 2023, 11:57 PM -04:00

--- Week 7: SQL - PSET & Lab/movies/11.sql
+++ Week 7: SQL - PSET & Lab/movies/11.sql
@@ -1,6 +1,6 @@
 SELECT title
 FROM movies
-JOIN ratings ON movies.id = rating.movie_id
+JOIN ratings ON movies.id = ratings.movie_id
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 WHERE name = "Chadwick Boseman"
--- Week 7: SQL - PSET & Lab/movies/13.sql
+++ Week 7: SQL - PSET & Lab/movies/13.sql
@@ -7,6 +7,9 @@ JOIN people p2 ON s2.person_id = p2.id
 JOIN movies m2 ON s2.movie_id = m2.id
 JOIN stars s3 ON m2.id = s3.movie_id
 JOIN people p3 ON s3.person_id = p3.id
+JOIN stars s4 ON m.id = s4.movie_id
+JOIN people p4 ON s4.person_id = p4.id
 WHERE p2.name = 'Kevin Bacon'
   AND p2.birth = 1958
-  AND p3.id <> p.id;
+  AND p3.id <> p.id
+  AND p4.name <> 'Kevin Bacon';
