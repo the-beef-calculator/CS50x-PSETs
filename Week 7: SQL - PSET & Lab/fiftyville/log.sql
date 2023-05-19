a8e88bad2d0e76cda30654bd1783fec5633cf3b5
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-19 18:54:02 +0000

    Fri, May 19, 2023, 2:54 PM -04:00

--- Week 7: SQL - PSET & Lab/fiftyville/log.sql
+++ Week 7: SQL - PSET & Lab/fiftyville/log.sql
@@ -6,5 +6,9 @@
 
 
 -- 2nd Query: SELECT description FROM crime_scene_reports WHERE street = "Humphrey Street" AND day = 28 AND month = 7 AND year = 2021;
--- Pull up the crime report for when the crime took place. Report says: "Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
- Littering took place at 16:36. No known witnesses.
+
+-- Query for  crime report for when the crime took place. Report says: "Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
+-- Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
+-- Littering took place at 16:36. No known witnesses.
+
+--
