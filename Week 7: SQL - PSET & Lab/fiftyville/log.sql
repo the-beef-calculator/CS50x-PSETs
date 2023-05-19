ac31ba7233f2aaa819652a1591f5169787f75a43
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-19 18:52:47 +0000

    Fri, May 19, 2023, 2:52 PM -04:00

--- Week 7: SQL - PSET & Lab/fiftyville/log.sql
+++ Week 7: SQL - PSET & Lab/fiftyville/log.sql
@@ -1,7 +1,10 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Crime info: Took place on July 28, 2021 on Humphrey Street.
--- 1st Query: .schema to get an idea of what i'm workin with
+-- 1st Query: .schema:
+-- Done to get an idea of what i'm working with.
 
 
--- 2nd Query:
+-- 2nd Query: SELECT description FROM crime_scene_reports WHERE street = "Humphrey Street" AND day = 28 AND month = 7 AND year = 2021;
+-- Pull up the crime report for when the crime took place. Report says: "Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
+ Littering took place at 16:36. No known witnesses.
