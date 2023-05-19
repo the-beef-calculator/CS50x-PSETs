18ebf527f2056cd67a4ecbffdfee6230c47f0c8c
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-19 03:18:55 +0000

    Thu, May 18, 2023, 11:18 PM -04:00

--- Week 7: SQL - PSET & Lab/movies/11.sql
+++ Week 7: SQL - PSET & Lab/movies/11.sql
@@ -1,6 +1,7 @@
-In 11.sql, write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
-
-    Your query should output a table with a single column for the title of each movie.
-    You may assume that there is only one person in the database with the name Chadwick Boseman.
-
-SELECT title FROM movies JOIN ratings ON movies.id = movie_id JOIN stars ON movies.id = movie_id JOIN people ON stars.person_id = people.id WHERE name = "Chadwick Boseman" ORDER BY rating;
+SELECT title
+FROM movies
+JOIN ratings ON movies.id = rating.movie_id
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id
+WHERE name = "Chadwick Boseman"
+ORDER BY rating;
