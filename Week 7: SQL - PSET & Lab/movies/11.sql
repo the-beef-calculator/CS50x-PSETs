15ccc28b660dd074530872a146240dde04e12188
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2023-05-19 04:03:46 +0000

    Fri, May 19, 2023, 12:03 AM -04:00

--- Week 7: SQL - PSET & Lab/movies/11.sql
+++ Week 7: SQL - PSET & Lab/movies/11.sql
@@ -1,7 +1,8 @@
-SELECT title
-FROM movies
-JOIN ratings ON movies.id = ratings.movie_id
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-WHERE name = "Chadwick Boseman"
-ORDER BY rating;
+SELECT m.title
+FROM movies m
+JOIN ratings r ON m.id = r.movie_id
+JOIN stars s ON m.id = s.movie_id
+JOIN people p ON s.person_id = p.id
+WHERE p.name = 'Chadwick Boseman'
+ORDER BY r.rating DESC
+LIMIT 5;
